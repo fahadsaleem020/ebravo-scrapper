9efe58f3335946634dd10527410ef5f1b7113249
palari <fahadsaleem020@gmail.com>
2023-04-11 14:55:33 +0500

    with thumbnails

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 import path from "path";
 
 const target = "http://ebravo.pk/classic/webtv?cat=All";
-const filePath = path.join(__dirname, "/iptv.m3u");
+const file = path.join(__dirname, "/iptv.m3u");
 const host = "http://ebravo.pk/classic/";
 const port = process.env.PORT || 3000;
 const app: Application = express();
@@ -14,17 +14,17 @@ const app: Application = express();
 interface Channel {
   name: string;
   url: string;
+  thumb: string;
 }
 
-app.use(timeout("600s"));
+app.use(timeout("300s"));
 
-app.get("/online", (req, res) => res.end("online"));
 app.get("/", async (req, res) => {
   try {
-    fs.unlink(filePath, (error) => null);
+    fs.unlink(file, (error) => null);
     const web = await (await axios(target)).data;
     await generateFile(await extractChannels(load(web)));
-    res.download(filePath);
+    res.download(file);
   } catch (error) {
     res.end((error as Error).message);
   }
@@ -36,7 +36,8 @@ const extractChannels = async ($: CheerioAPI) => {
     const source = (await axios(host + anchor.attribs.href)).data;
     const url = load(source)("video source").get(0)!.attribs!.src;
     const name = url.split("/")[4].split(".")[0];
-    channels.push({ url, name });
+    const thumb = host + load(anchor)("img").attr("src");
+    channels.push({ url, name, thumb });
   }
   return channels;
 };
@@ -44,8 +45,12 @@ const extractChannels = async ($: CheerioAPI) => {
 const generateFile = (channels: Channel[]): Promise<string> => {
   return new Promise((resolve, reject) => {
     channels.forEach((channel, i) => {
-      const content = `\n\n#EXTINF:${i + 1}, ${channel.name} \n${channel.url}`;
-      fs.appendFile(filePath, content, (err) => {
+      const content = `\n\n#EXTINF:0 tvg-id="channel${
+        i + 1
+      }" tvg-name="Channel ${i + 1}" tvg-logo="${channel.thumb}", ${
+        channel.name
+      } \n${channel.url}`;
+      fs.appendFile(file, content, (err) => {
         if (err) {
           reject((err as Error).message);
         }
